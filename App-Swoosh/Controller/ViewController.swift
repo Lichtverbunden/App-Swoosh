4ff8ca2070f6f1e15c4e6d505f7e1a14da202102
Ken Krippeler <kriken19@gmail.com>
2017-08-11 21:50:50 +0200

    First clean up

---
+++ App-Swoosh/Controller/ViewController.swift
@@ -0,0 +1,23 @@
+//
+//  ViewController.swift
+//  App-Swoosh
+//
+//  Created by Ken Krippeler on 10.08.17.
+//  Copyright © 2017 Lichtverbunden. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController
+{
+
+    override func viewDidLoad()
+    {
+        super.viewDidLoad()
+        
+    }
+
+    
+
+}
+
